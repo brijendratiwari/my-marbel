d987b5a9bed6549c864b0d56c30ae7e1611e2649
abhay <mayank@ignisitsolutions.com>
2016-06-22 13:46:12 +0530

    contact us implement ion

--- application/controllers/customer/Support.php
+++ application/controllers/customer/Support.php
@@ -9,6 +9,8 @@ class Support extends CI_Controller {
         $this->load->database();
         $this->load->model('customers_model', "Customer");
         $this->load->model('users_model', "Users");
+        $this->load->model('services_model', "Services");
+        $this->load->library('My_phpmailer');
         if ($this->Users->auth_check() == false) {
             redirect('/login');
         }
@@ -20,4 +22,72 @@ class Support extends CI_Controller {
       
         $this->load->customer('customer/support',$this->data);
     }
+    public function upload(){
+        $this->load->helper('my_mail');
+        $orders = $this->Services->getOrders($this->session->userdata['marbel_user']['user_id']);
+        if (isset($_FILES["file"]) && $_FILES["file"]["error"]== UPLOAD_ERR_OK) {
+                    $uploadDirectory = CONTACT_UPLOADS_DIRECTORY;
+                    if (!isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+                        die();
+                    }
+
+                    if ($_FILES["file"]["size"] > CONTACT_ALLOWED_FILESIZE) {
+                        die("File size is too big!");
+                    }
+
+                    switch (strtolower($_FILES['file']['type'])) {
+                        case 'image/png': 
+                        case 'image/gif': 
+                        case 'image/jpeg': 
+                        case 'image/pjpeg':
+                        case 'text/plain':
+                        case 'text/html':
+                        case 'application/x-zip-compressed':
+                        case 'application/pdf':
+                        case 'application/msword':
+                        case 'application/vnd.ms-excel':
+                        case 'video/mp4':
+                        break;
+                        default:
+                        die('Unsupported File!'); 
+                    }
+
+                    $fileName = strtolower($_FILES['file']['name']);
+                    $fileExt = substr($fileName, strrpos($fileName, '.'));
+                    $rand = rand(0, 9999999999);
+                    $newFileName = $this->session->userdata['marbel_user']['user_id'].'-'.$rand.$fileExt;
+                    file_put_contents($uploadDirectory.'/data.log', $uploadDirectory.$newFileName."\n", FILE_APPEND);
+                    if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadDirectory.$newFileName )) {
+                        $from = $this->session->userdata['marbel_user']['email'];
+                        $fromName = $this->session->userdata['marbel_user']['first_name'].' '.$this->session->userdata['marbel_user']['last_name'];
+                        $subject = $fromName.' has submitted a support form VIA '.base_url().'customer/support';
+                        $text = "Message: ".$this->input->post('cd-notes')."\n\n";
+                        $text .= "Uploaded Attachment: ".base_url('assets/uploads/').$newFileName."\n\n";
+                        $mail = mymail('sandeep@ignisitsolutions.com',$subject, $text,FALSE,$from,$fromName);
+                        if(!$mail) {
+                            die ($mail);
+                        } else {
+                            die('Message has been sent');
+                        }
+                    } else {
+                        die('Error uploading file!');
+                    }    
+        } else if ($this->input->post('cd-notes')) {
+                $from = $this->session->userdata['marbel_user']['email'];
+                $fromName = $this->session->userdata['marbel_user']['first_name'].' '.$this->session->userdata['marbel_user']['last_name'];
+                $subject = $fromName.' has submitted a support form VIA'. base_url().'/customer/support';
+                $text = "Message: ".$this->input->post('cd-notes')."\n\n";
+                $text .= "Orders:";
+                $mail = mymail(CONTACT_EMAIL,$subject, $text,FALSE,$from,$fromName);
+                        if(!$mail) {
+                            die ($mail);
+                        } else {
+                            die('Message has been sent');
+                        }
+        } else {
+                 die('Something wrong with upload!');
+        }
+    }
+    
+    
 }
--- application/views/customer/support.php
+++ application/views/customer/support.php
@@ -25,12 +25,12 @@
 
             <tr>
               <td width="100%" align="center" valign="top">
-                <form id="contactForm" class="cd-form floating-labels" enctype="multipart/form-data" method="POST" action="support">
+                <form id="contactForm" class="cd-form floating-labels" enctype="multipart/form-data" method="POST" action="upload">
                   <fieldset>
                     <div class="form-group">
                       <div class="row-fluid">
                         <div class="col-md-12">
-                          <textarea placeholder="What's on your mind..." class="form-control" name="cd-notes" id="cd-notes" style="margin-top: -100px" required></textarea>
+                          <textarea placeholder="What's on your mind..."  name="cd-notes" id="cd-notes" style="margin-top: -100px" required></textarea>
                         </div>
                       </div>
                     </div>
@@ -84,3 +84,71 @@
       </table>
       <!--- End of notes section --> 
 </div>
+    
+    <script>
+$(document).ready(function() { 
+  var options = { 
+    target:   '#output',
+    beforeSubmit:  beforeSubmit,
+    success:       afterSuccess,
+    uploadProgress: OnProgress,
+    resetForm: true
+  };
+
+  function beforeSubmit() {
+    $('#output').show();
+    $('#output').addClass("alert-warning");
+    $('#output').text('Starting Upload...');
+  }
+
+  function afterSuccess() {
+    $('#submit-btn').val('Submit');
+  }
+  function OnProgress(event, position, total, percentComplete) {
+    $('#output').text('Uploaded '+percentComplete + '%');
+    if (percentComplete >= 99) {
+      setTimeout(function() {
+        $('#output').hide('slow');
+      }, 5000);
+    }
+  }
+
+  $('#contactForm').on('submit', function(e) {
+    e.preventDefault();
+    $(this).ajaxSubmit(options);
+  });
+});
+
+$(document).ready(function () {
+  $("#date-popover").popover({html: true, trigger: "manual"});
+  $("#date-popover").hide();
+  $("#date-popover").click(function (e) {
+    $(this).hide();
+  });
+
+//  $("#my-calendar").zabuto_calendar({
+//    action: function () {
+//      return myDateFunction(this.id, false);
+//    },
+//    action_nav: function () {
+//      return myNavFunction(this.id);
+//    },
+//    ajax: {
+//      url: "show_data.php?action=1",
+//      modal: true
+//    },
+//    legend: [
+//    {type: "text", label: "Special event", badge: "00"},
+//    {type: "block", label: "Regular event", }
+//    ]
+//  });
+});
+
+
+function myNavFunction(id) {
+  $("#date-popover").hide();
+  var nav = $("#" + id).data("navigation");
+  var to = $("#" + id).data("to");
+  console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
+}
+</script>
