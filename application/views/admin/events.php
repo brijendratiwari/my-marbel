77bca99160c6eedeae56c4c7482e4023881c3780
ignis <ignis@ignis.com>
2016-07-05 16:19:51 +0530

    css

--- application/views/admin/events.php
+++ application/views/admin/events.php
@@ -1,9 +1,9 @@
  <?php if(!empty($events)){ 
                                 foreach($events as $key=>$val){
                                 ?>
-                            <div class="col-lg-12 form-group">
-                            <div class="col-lg-9 get-event" data-eventId='<?php echo $val['id']; ?>'  data-target="#editEventModal" data-toggle="modal">
-                                <i class="fa fa-calendar"></i> <a href="#"><?php echo substr($val['title'],0,9) . '...'; ?></a>
+                            <div class="col-lg-12 form-group"  style="font-size:12px;font-weight:bold;">
+                            <div class="col-lg-9 get-event"  data-eventId='<?php echo $val['id']; ?>'  data-target="#editEventModal" data-toggle="modal">
+                                <i class="fa fa-calendar"></i> <a href="#" style="color:black;"><?php echo substr($val['title'],0,10) . '...'; ?></a>
                             </div>
                             <div class="col-lg-3">
                                 <a href="#"  class="text-danger"><i class="fa fa-trash"></i></a>
