8fb8a031a0a9108cfa26a0f0d09c4f449a383693
abhay <mayank@ignisitsolutions.com>
2016-07-13 19:37:21 +0530

    remove nav profile image

--- application/views/_layouts/nav.php
+++ application/views/_layouts/nav.php
@@ -1,14 +1,6 @@
  <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
-                <p class="centered">
-                  <a href="<?php echo base_url('profile');?>">
-                <img class="img-circle" width="60" src="/assets/img/ui-sam.jpg">
-                </a>
-                </p>
-                <h5 class="centered"><?php if($this->session->userdata('marbel_user')){
-                    
-                    echo $this->session->userdata['marbel_user']['first_name']." ".$this->session->userdata['marbel_user']['last_name'];
-                } ?></h5>
+             
                     <ul class="nav" id="side-menu">
                         <li>
                             <a href="javascript:;"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
