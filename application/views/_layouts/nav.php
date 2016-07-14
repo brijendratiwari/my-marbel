065e8dd4aac7afa20802caa906e530b4462f64e9
PRATEEK <prateek@test.com>
2016-07-14 12:23:57 +0530

    delete logo

--- application/views/_layouts/nav.php
+++ application/views/_layouts/nav.php
@@ -1,15 +1,7 @@
  <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
 
-                <p class="centered">
-                  <a href="<?php echo base_url('profile');?>">
-                <img class="img-circle" width="60" src="../assets/img/ui-sam.jpg">
-                </a>
-                </p>
-                <h5 class="centered"><?php if($this->session->userdata('marbel_user')){
-                    
-                    echo $this->session->userdata['marbel_user']['first_name']." ".$this->session->userdata['marbel_user']['last_name'];
-                } ?></h5>
+              
 
                     <ul class="nav" id="side-menu">
                         <li>
